24dede0108625f2cf9e8db8d78523193b89c1a17
Serhii Kis <4irius@gmail.com>
2020-06-09 12:45:32 +0300

    AppHeader

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader= () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Serhii's Account</h1>
+            <h2>5 записів, з них сподобалось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
